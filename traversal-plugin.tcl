87792275185c09d64c1378e90c3345c7f403d3c0
monetus <UFOsFollowMe@gmail.com>
2018-03-18 18:18:50 -0400

    prevent a divide by zero error
    added max(1, ...) to the index increment within the next_obj proc

--- traversal-plugin.tcl
+++ traversal-plugin.tcl
@@ -6,8 +6,9 @@ proc ::pdtk_canvas::next_atom {tkcanvas x y} {
   pdtk_canvas_mouseup $tkcanvas $x $y 1
       # find all the atoms.
   set atom_ids [$tkcanvas find withtag {atom&&text}]
-      # increment upon the last stored index, but keep it within the bounds of the list with a modulo
-  set ::pdtk_canvas::atom_index [expr { (1 + $::pdtk_canvas::atom_index) % [llength $atom_ids] }]
+      # increment upon the last stored index, but keep it within the bounds of the list with a modulo\
+          note that the max(1, llength) is to keep the modulo from producing a divide by zero error
+  set ::pdtk_canvas::atom_index [expr { (1 + $::pdtk_canvas::atom_index) % max(1, [llength $atom_ids]) }]
       # get the id number the coords command needs to look for
   set atom [lindex $atom_ids $::pdtk_canvas::atom_index]
       # set the new x and y coordinates to the location of the new atom
