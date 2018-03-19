a9dc084d55ab5f6cc89a8c4d92f4960e2221dfad
monetus <UFOsFollowMe@gmail.com>
2018-03-18 21:25:48 -0400

    replaced max(1,...) with an if statement that encompasses the rest of the proc
    rather than protect from one error, logically deal with the fact that an empty list shouldn't be processed.

--- traversal-plugin.tcl
+++ traversal-plugin.tcl
@@ -6,16 +6,18 @@ proc ::pdtk_canvas::next_atom {tkcanvas x y} {
   pdtk_canvas_mouseup $tkcanvas $x $y 1
       # find all the atoms.
   set atom_ids [$tkcanvas find withtag {atom&&text}]
-      # increment upon the last stored index, but keep it within the bounds of the list with a modulo\
-          note that the max(1, llength) is to keep the modulo from producing a divide by zero error
-  set ::pdtk_canvas::atom_index [expr { (1 + $::pdtk_canvas::atom_index) % max(1, [llength $atom_ids]) }]
-      # get the id number the coords command needs to look for
-  set atom [lindex $atom_ids $::pdtk_canvas::atom_index]
-      # set the new x and y coordinates to the location of the new atom
-  lassign [$tkcanvas coords $atom] x y
-      # click on the new atom.  FYI, the 1 is for mousebutton one and the 0 is for not holding shift.
-  pdtk_canvas_mouse $tkcanvas $x $y 1 0
-  pdtk_canvas_mouseup $tkcanvas $x $y 1
+  set size [llength $atom_ids]
+  if {$size > 0} {
+        # increment upon the last stored index, but keep it within the bounds of the list with a modulo
+    set ::pdtk_canvas::atom_index [expr { (1 + $::pdtk_canvas::atom_index) % $size}]
+        # get the id number the coords command needs to look for
+    set atom [lindex $atom_ids $::pdtk_canvas::atom_index]
+        # set the new x and y coordinates to the location of the new atom
+    lassign [$tkcanvas coords $atom] x y
+        # click on the new atom.  FYI, the 1 is for mousebutton one and the 0 is for not holding shift.
+    pdtk_canvas_mouse $tkcanvas $x $y 1 0
+    pdtk_canvas_mouseup $tkcanvas $x $y 1
+  }
 }
 
     # make this event special so other gui-plugins can bind to it.
